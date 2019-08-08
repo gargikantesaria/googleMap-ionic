e5e93cf63bec66025d5a819a813f20232970fa9b
Gargi.kantesaria <Gargi.kantesaria@solutionanalysts.com>
2019-08-08 10:48:15 +0530

    initital commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { GoogleMap, GoogleMapOptions, GoogleMaps, Marker, GoogleMapsEvent } from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,45 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  map: GoogleMap;
 
+  constructor() {
+  }
+
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+
+   addMarker(){
+  //   this will load the marker on the map
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 23.069491199999998,
+        lng: 72.53196799999999
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert(' Marker has been clicked!!');
+    });
+  }
+  loadMap() {
+    // This code is necessary for browser
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: 23.069491199999998,
+           lng: 72.53196799999999
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
   }
 
 }
